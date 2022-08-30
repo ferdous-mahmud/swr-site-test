f55fe508c8b8f305a3099071e86064cfdb051cb4
ferdous-mahmud <ferdous.webdev@gmail.com>
2022-08-30 08:31:17 +0600

    modify blog component

--- components/blog-index.js
+++ components/blog-index.js
@@ -6,20 +6,20 @@ export default function BlogIndex({ more = "Read more" }) {
     // Alias `<a>` to avoid it being replaced by MDX components.
     const A = "a";
     return (
-      <div className="">
-        <h3>
+      <div className="bg-gray-300 p-3 rounded-md">
+        <h3 className="dark:text-black -mt-1 -mb-3">
           <Link href={page.route}>
             <A style={{ color: "inherit", textDecoration: "none" }}>
               {page.meta?.title || page.frontMatter?.title || page.name}
             </A>
           </Link>
         </h3>
-        <p className="opacity-80">
+        <p className="opacity-90 dark:text-gray-900">
           {page.frontMatter?.description}{" "}
           <Link href={page.route}>{more + " →"}</Link>
         </p>
         {page.frontMatter?.date ? (
-          <p className="opacity-50 text-sm">{page.frontMatter.date}</p>
+          <p className="opacity-60 text-sm dark:text-gray-900">{page.frontMatter.date}</p>
         ) : null}
       </div>
     );
